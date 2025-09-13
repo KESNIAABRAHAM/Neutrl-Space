48d6f28494385a6e63e6c8b454e7c74239d0393a
Your Name <uchendukesnia2424@gmail.com>
2025-09-13 17:18:36 +0100

    MSI

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 //Home hero section smooth scroll animation
-document.addEventListener('DOMContentLoaded', () => {
-  const cta = document.getElementById('cta');
-  const nextSection = document.getElementById('editorial');
+document.addEventListener("DOMContentLoaded", () => {
+  const cta = document.getElementById("cta");
+  const nextSection = document.getElementById("editorial");
 
-  cta.addEventListener('click', () => {
-    nextSection.scrollIntoView({ behavior: 'smooth' });
+  cta.addEventListener("click", () => {
+    nextSection.scrollIntoView({ behavior: "smooth" });
   });
 });
 
-
 //Rendering product cards via JSON
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('data/products.json')
-    .then(res => res.json())
-    .then(products => {
-      const grid = document.getElementById('products-grid');
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("data/products.json")
+    .then((res) => res.json())
+    .then((products) => {
+      const grid = document.getElementById("products-grid");
 
-      products.forEach(product => {
-        const card = document.createElement('div');
-        card.className = 'product-card';
+      products.forEach((product) => {
+        const card = document.createElement("div");
+        card.className = "product-card";
 
         card.innerHTML = `
           <div class="product-image">
-            <img src="${product.primaryImage}" alt="${product.title}" class="primary" loading="lazy">
-            ${product.secondaryImage ? `<img src="${product.secondaryImage}" alt="${product.title} alt" class="secondary" loading="lazy">` : ''}
+            <img src="${product.primaryImage}" alt="${
+          product.title
+        }" class="primary" loading="lazy">
+            ${
+              product.secondaryImage
+                ? `<img src="${product.secondaryImage}" alt="${product.title} alt" class="secondary" loading="lazy">`
+                : ""
+            }
             <div class ="hover-text">ADD TO CART</div>
             <span class="product-tag">${product.tag}</span>
           </div>    
@@ -36,40 +41,60 @@ document.addEventListener('DOMContentLoaded', () => {
         grid.appendChild(card);
       });
     })
-    .catch(err => console.error('Error loading products:', err));
+    .catch((err) => console.error("Error loading products:", err));
 });
 
 //Email Signup Logic
-// document.addEventListener('DOMContentLoaded', () => {
-//   const form = document.getElementById('email-capture');
-//   const input = document.getElementById('email-input');
-//   const message = document.getElementById('email-message');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("email-capture");
+  const input = document.getElementById("email-input");
+  const message = document.getElementById("email-message");
 
-//   // Check if already signed up
-//   if (localStorage.getItem('signedUp') === 'true') {
-//     form.style.display = 'none';
-//     message.className = "email-message success"
-//     message.textContent = 'You’re already signed up. Welcome back!'
-//     return;
-//   }
+  // Check if already signed up
+  if (localStorage.getItem("signedUp") === "true") {
+    form.style.display = "none";
+    message.className = "email-message success";
+    message.textContent = "You’re already signed up. Welcome back!";
+    return;
+  }
 
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const email = input.value.trim();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = input.value.trim();
 
-//     // Simple email validation
-//     const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    // Simple email validation
+    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-//     if (!isValid) {
-//       message.className = "email-message error"
-//       message.textContent = 'Please enter a valid email address.';
-//       return;
-//     }
+    if (!isValid) {
+      message.className = "email-message error";
+      message.textContent = "Please enter a valid email address.";
+      return;
+    }
 
-//     // Persist flag and show success
-//     localStorage.setItem('signedUp', 'true');
-//     form.style.display = 'none';
-//     message.textContent = 'Thank you for joining The Reserve!';
-//     message.className = 'email-message success';
-//   });
-// });
+    // Persist flag and show success
+    localStorage.setItem("signedUp", "true");
+    form.style.display = "none";
+    message.textContent = "Thank you for joining The Reserve!";
+    message.className = "email-message success";
+  });
+});
+
+const menuBtn = document.querySelector(".nav-left .icon-btn");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeSidebar");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.add("open");
+  overlay.classList.add("visible");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("visible");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("visible");
+});
